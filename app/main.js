9fe2eaeafefb5867efcb6f170edee30f957c6bc9
Adam Dzięcioł <wolapark@gmail.com>
2018-11-18 14:39:50 +0100

    commit

--- app/main.js
+++ app/main.js
@@ -1,1 +1,17 @@
-let images = [];
+
+let imagesList = new ImagesList({source: localData});
+imagesList.render();
+let fileForm = new FileForm();
+fileForm.render();
+fileForm.bindEvent();
+
+// allImages = [];
+// let parentEL = document.querySelector(".images-list");
+// allImages = localData.map(function(image) {
+   
+//   let imageClass = new ImageClass(image);
+//   return parentEL.appendChild(imageClass.render());
+
+//   //  return `<img src='${image.url}'/>`;
+// });
+
